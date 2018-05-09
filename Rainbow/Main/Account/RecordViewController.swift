1ea9e545e74c9776acdd933949a96ea87a899de0
cyrusblaer <cyrusblaer@hotmail.com>
2018-05-09 19:56:02 +0800

    Record View

--- Rainbow/Main/Account/RecordViewController.swift
+++ Rainbow/Main/Account/RecordViewController.swift
@@ -8,19 +8,59 @@
 
 import UIKit
 
-class RecordViewController: UIViewController {
+class RecordViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    // MARK: - Properties
+    @IBOutlet weak var recordTableView: UITableView!
+    @IBOutlet weak var noDataLabel: UILabel!
+    @IBOutlet weak var noDataImage: UIImageView!
+    
+    var haveData : Bool = false
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.recordTableView.delegate = self
+        self.recordTableView.dataSource = self
+        self.recordTableView.tableFooterView = UIView()
+        
+        if self.haveData {
+            self.recordTableView.isHidden = false
+            self.noDataImage.isHidden = true
+            self.noDataLabel.isHidden = true
+        }else {
+            self.recordTableView.isHidden = true
+            self.noDataImage.isHidden = false
+            self.noDataLabel.isHidden = false
+        }
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.setNavigationBarHidden(false, animated: true)
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    //  MARK: - Delegate
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "recordCell", for: indexPath)
+        cell.selectionStyle = .none
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 
     /*
     // MARK: - Navigation
